9544f8390d2e31cafab6018a97d732bedfb2d122
DikeH-Ai <56478645+DikeH-Ai@users.noreply.github.com>
2023-04-25 19:03:10 -0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
+ * _printf - formatted output conversion, print data.
  * @format: input string.
- *
- * Return: number of chars printed.
+ * Return: number of characters printed.
  */
 int _printf(const char *format, ...)
 {
